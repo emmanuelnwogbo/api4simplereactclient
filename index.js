d818b966293eda8713c73c9900c60b62ba21648b
Emmanuel Nwogbo <emmnlchuck@gmail.com>
2017-07-26 17:48:22 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+const mongoose = require('mongoose')
+
+const app = express();
+
+const router = require('./router');
+
+mongoose.connect('mongodb://localhost:auth/authreact');
+
+app.use(morgan('combined'));
+app.use(bodyParser.json({ type: '*/*' }));
+router(app);
+
+const port = process.env.PORT || 3090;
+const server = http.createServer(app);
+server.listen(port);
+console.log('server listening on:', port)
